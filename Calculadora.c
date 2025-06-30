c68aa9779a20e1044a368bf18a7cba7769d1e3d2
Tensai33x <spaikesamtll@gmail.com>
2025-06-30 05:54:47 -0300

    ss2

--- Calculadora.c
+++ Calculadora.c
@@ -3,6 +3,7 @@
 
 int main(){
 <<<<<<< HEAD
+<<<<<<< HEAD
 setlocale(LC_ALL,"portuguese");
 float num1, nu, res;
 char ex;
@@ -11,22 +12,47 @@ char ex;
     printf("Digite o Primeiro N�mero: ");
         if (scanf("%f", &num1) != 1) {
             printf("\n\nEntrada inv�lida! Digite apenas n�meros\n");
+=======
+int num1, nu, res;
+char ex;
+    printf("numero 1: ");
+        if (scanf("%d", &num1) != 1) {
+            printf("Entrada invalida! Digite apenas: numeros\n");
+>>>>>>> parent of 1c9d913 (versão 3.0)
             fflush(stdin);
-            goto num1;} 
+            system("pause");
+            system("cls");
+            main();} 
         fflush(stdin);
+<<<<<<< HEAD
     fator:
     printf("Digite o Operador: ");
         if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
             printf("\n\nOperador inv�lido! Digite: +, -, * ou /.\n");
+=======
+    printf("\nfator: ");
+        if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
+            printf("Entrada invalida! Digite apenas +, -, * ou /.\n");
+>>>>>>> parent of 1c9d913 (versão 3.0)
             fflush(stdin);
-            goto fator;}
+            system("pause");
+            system("cls");
+            main();}
         fflush(stdin);
+<<<<<<< HEAD
     num2:
     printf("Digite o Segundo N�mero: ");
         if (scanf("%f", &nu) != 1){
             printf("\n\nEntrada inv�lida! Digite apenas n�meros\n");
+=======
+    printf("\nnumero 2: ");
+        if (scanf("%d", &nu) != 1){
+            printf("Entrada invalida! Digite apenas: numeros\n");
+>>>>>>> parent of 1c9d913 (versão 3.0)
             fflush(stdin);
-            goto num2;} 
+            system("pause");
+            system("cls");
+            main();} 
         fflush(stdin);
 switch (ex){
     case '+':
@@ -44,6 +70,7 @@ switch (ex){
     default:   
         break;
     }
+<<<<<<< HEAD
     printf("\nResultado:%.2f\n",res);
     printf("Pressione ENTER para reiniciar a calculadora.");
     getchar();
@@ -57,3 +84,10 @@ switch (ex){
     
 }
 >>>>>>> parent of e82f3b3 (Revert "test")
+=======
+    printf("\nResultado:%d\n",res);
+    system("pause");
+    system("cls");
+    main();
+}
+>>>>>>> parent of 1c9d913 (versão 3.0)
