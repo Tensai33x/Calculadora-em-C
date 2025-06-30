277369d90155a73c89246cdf92d735270e898ccb
Tensai33x <spaikesamtll@gmail.com>
2025-06-30 03:02:47 -0300

    Modificações

--- Calculadora.c
+++ Calculadora.c
@@ -2,8 +2,50 @@
 #include <stdlib.h>
 
 int main(){
-
-
-
-    
+int num1, nu, res;
+char ex;
+    printf("numero 1: ");
+        if (scanf("%d", &num1) != 1) {
+            printf("Entrada invalida! Digite apenas: numeros\n");
+            fflush(stdin);
+            system("pause");
+            system("cls");
+            main();} 
+        fflush(stdin);
+    printf("\nfator: ");
+        if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
+            printf("Entrada invalida! Digite apenas +, -, * ou /.\n");
+            fflush(stdin);
+            system("pause");
+            system("cls");
+            main();}
+        fflush(stdin);
+    printf("\nnumero 2: ");
+        if (scanf("%d", &nu) != 1){
+            printf("Entrada invalida! Digite apenas: numeros\n");
+            fflush(stdin);
+            system("pause");
+            system("cls");
+            main();} 
+        fflush(stdin);
+switch (ex){
+    case '+':
+        res = num1 + nu; 
+        break;
+    case '/':
+        res = num1 / nu;    
+        break;
+    case '*':
+        res = num1 * nu;       
+        break;
+    case '-':
+        res = num1 - nu;       
+        break;  
+    default:   
+        break;
+    }
+    printf("\nResultado:%d\n",res);
+    system("pause");
+    system("cls");
+    main();
 }
