9fc1fafe175ff514a7792cd61e89c4b540979b82
Tensai33x <spaikesamtll@gmail.com>
2025-06-30 05:57:59 -0300

    7.0

--- Calculadora.c
+++ Calculadora.c
@@ -1,58 +1,32 @@
 #include  <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main(){
-<<<<<<< HEAD
-<<<<<<< HEAD
 setlocale(LC_ALL,"portuguese");
 float num1, nu, res;
 char ex;
     system("cls");
 	num1:
-    printf("Digite o Primeiro Nï¿½mero: ");
+    printf("Digite o Primeiro Número: ");
         if (scanf("%f", &num1) != 1) {
-            printf("\n\nEntrada invï¿½lida! Digite apenas nï¿½meros\n");
-=======
-int num1, nu, res;
-char ex;
-    printf("numero 1: ");
-        if (scanf("%d", &num1) != 1) {
-            printf("Entrada invalida! Digite apenas: numeros\n");
->>>>>>> parent of 1c9d913 (versÃ£o 3.0)
+            printf("\n\nEntrada inválida! Digite apenas Números\n");
             fflush(stdin);
-            system("pause");
-            system("cls");
-            main();} 
+            goto num1;} 
         fflush(stdin);
-<<<<<<< HEAD
     fator:
     printf("Digite o Operador: ");
         if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
-            printf("\n\nOperador invï¿½lido! Digite: +, -, * ou /.\n");
-=======
-    printf("\nfator: ");
-        if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
-            printf("Entrada invalida! Digite apenas +, -, * ou /.\n");
->>>>>>> parent of 1c9d913 (versÃ£o 3.0)
+            printf("\n\nOperador inválido! Digite: +, -, * ou /.\n");
             fflush(stdin);
-            system("pause");
-            system("cls");
-            main();}
+            goto fator;}
         fflush(stdin);
-<<<<<<< HEAD
     num2:
-    printf("Digite o Segundo Nï¿½mero: ");
+    printf("Digite o Segundo Número: ");
         if (scanf("%f", &nu) != 1){
-            printf("\n\nEntrada invï¿½lida! Digite apenas nï¿½meros\n");
-=======
-    printf("\nnumero 2: ");
-        if (scanf("%d", &nu) != 1){
-            printf("Entrada invalida! Digite apenas: numeros\n");
->>>>>>> parent of 1c9d913 (versÃ£o 3.0)
+            printf("\n\nEntrada inválida! Digite apenas Números\n");
             fflush(stdin);
-            system("pause");
-            system("cls");
-            main();} 
+            goto num2;} 
         fflush(stdin);
 switch (ex){
     case '+':
@@ -70,24 +44,9 @@ switch (ex){
     default:   
         break;
     }
-<<<<<<< HEAD
     printf("\nResultado:%.2f\n",res);
     printf("Pressione ENTER para reiniciar a calculadora.");
     getchar();
     system("cls");
     main();
 }
-=======
-
-
-
-    
-}
->>>>>>> parent of e82f3b3 (Revert "test")
-=======
-    printf("\nResultado:%d\n",res);
-    system("pause");
-    system("cls");
-    main();
-}
->>>>>>> parent of 1c9d913 (versÃ£o 3.0)
