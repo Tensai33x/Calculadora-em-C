50c4f24e0f3b364b54180bd2ed11c6381b1468ea
Tensai33x <spaikesamtll@gmail.com>
2025-06-30 06:05:54 -0300

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -8,23 +8,23 @@ float num1, nu, res;
 char ex;
     system("cls");
 	num1:
-    printf("Digite o Primeiro N?mero: ");
+    printf("Digite o Primeiro Número: ");
         if (scanf("%f", &num1) != 1) {
-            printf("\n\nEntrada inv?lida! Digite apenas N?meros\n");
+            printf("\n\nEntrada inválida! Digite apenas Números\n");
             fflush(stdin);
             goto num1;} 
         fflush(stdin);
     fator:
     printf("Digite o Operador: ");
         if (scanf(" %c",&ex)!=1 || (ex != '+' && ex != '-' && ex != '*' && ex != '/')){
-            printf("\n\nOperador inv?lido! Digite: +, -, * ou /.\n");
+            printf("\n\nOperador inválido! Digite: +, -, * ou /.\n");
             fflush(stdin);
             goto fator;}
         fflush(stdin);
     num2:
-    printf("Digite o Segundo N?mero: ");
+    printf("Digite o Segundo Número: ");
         if (scanf("%f", &nu) != 1){
-            printf("\n\nEntrada inv?lida! Digite apenas N?meros\n");
+            printf("\n\nEntrada inválida! Digite apenas Números\n");
             fflush(stdin);
             goto num2;} 
         fflush(stdin);
